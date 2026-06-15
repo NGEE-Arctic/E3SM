2112968e316c961b0cd87381a501e7932cc2648f
Rich Fiorella <rfiorella@lanl.gov>
2026-06-14 21:02:42 -0600

    wip - debug high tk

--- components/elm/src/biogeophys/SoilTemperatureMod.F90
+++ components/elm/src/biogeophys/SoilTemperatureMod.F90
@@ -1041,6 +1041,8 @@ contains
                      thk(c,j) = exp(f_exice*log(tkice) + (1._r8-f_exice)*log(thk(c,j)))
                      ! thk(c,j) = 1._r8 / ((1._r8-f_exice)/thk(c,j) + f_exice/tkice) ! old parallel implementation
                   endif
+                  ! Modify thk for excess ice using sum of resistances
+                  thk(c,j) = 1._r8 / ((1._r8-f_exice)/thk(c,j)) + f_exice/tkice
                   if (j > nlevbed) thk(c,j) = thk_bedrock
                else if (lun_pp%itype(l) == istice .OR. lun_pp%itype(l) == istice_mec) then
                   thk(c,j) = tkwat
